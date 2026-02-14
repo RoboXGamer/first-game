908ee386fe78655e2b8741b1fda91dc82dbdd9da
Sreenandh S <ssreenandh069@gmail.com>
2026-02-14 12:05:39 +0530

    Added Main Menu (Not refined)

---
+++ scripts/main_menu.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
+
+
+func _on_continue_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
+
+
+func _on_options_pressed() -> void:
+	print("Menu Opened")
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
