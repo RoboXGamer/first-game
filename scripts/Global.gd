e7e192ae1ee862860ce757ba2cb1b238117c42a6
Sreenandh S <ssreenandh069@gmail.com>
2026-02-21 18:28:51 +0530

    Some global changes added

--- scripts/Global.gd
+++ scripts/Global.gd
@@ -2,6 +2,12 @@ extends Node
 
 var last_door_id : String = ""
 var Money = 1500
+var Money_Deposited = 0
+var Loan_Interest_Rate = 9
+var Day = 1
+var Frames = 0
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
