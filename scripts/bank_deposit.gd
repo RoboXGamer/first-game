9f6bc4860b53f70a8ce6f0e099e4902055fedde0
Sreenandh S <ssreenandh069@gmail.com>
2026-02-26 20:10:43 +0530

    added bank deposit window

--- scripts/bank_deposit.gd
+++ scripts/bank_deposit.gd
@@ -1,21 +1,20 @@
 extends TextureRect
 
+@onready var keypad = $Keypad
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	keypad.num_confirm.connect(_on_keypad_num_confirmed)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+func _on_keypad_num_confirmed(number : int)-> void:
+	Global.Money-=int(number)
+	Global.Money_Deposited+=int(number)
 
 
 func _on_exit_pressed() -> void:
-	Global.last_door_id="WithdrawSpawn"
-	SceneTransition.change_scene("res://scenes/bank_interior.tscn")
-
-
-func _on_deposit_pressed() -> void:
-	$"../Deposit_Window".show()
 	$".".hide()
+	$"../Bank_Menu".show()
--- scripts/keypad.gd
+++ scripts/keypad.gd
@@ -1,6 +1,6 @@
 extends Control
 
-
+signal num_confirm(final_num: String)
 var num = ''
 var num_len = 0
 # Called when the node enters the scene tree for the first time.
@@ -18,9 +18,12 @@ func _on_number_pressed(extra_arg_0: int) -> void:
 	num+=str(extra_arg_0)
 
 func _on_delete_pressed() -> void:
+	if num_len<=0:
+		return
 	num=num.substr(0,num_len-1)
 	num_len-=1
-	print(num)
 
 func _on_confirm_pressed() -> void:
-	print(num)
+	num_confirm.emit(int(num))
+	num=''
+	num_len=0
